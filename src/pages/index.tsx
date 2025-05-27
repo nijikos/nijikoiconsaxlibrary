f3cfedd6105c8db6314bc1124143ed55dbe28081
nijikos <nijikos@gmail.com>
2025-05-27 14:49:37 +0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,76 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import IconsaxBold2, {
+  iconsBold2,
+} from "@/components/Icon/IconsaxBoldComplete";
+import Iconsax2, { icons2 } from "@/components/Icon/IconsaxComplete";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
-const inter = Inter({ subsets: ['latin'] })
+import React from "react";
 
-export default function Home() {
+export default function iconsax() {
+  let iconNames2 = Object.keys(icons2);
+  let iconNamesBold2 = Object.keys(iconsBold2);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='bg-n-0 w-full h-full'>
+      <TabGroup>
+        <TabList className='flex gap-4'>
+          <Tab
+            key={"linear"}
+            className='rounded-full px-3 py-1 text-sm/6 font-semibold text-gray-900 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-gray-900 data-hover:bg-gray-100 data-selected:bg-gray-200 data-selected:data-hover:bg-gray-200'
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Iconsax
+          </Tab>
+          <Tab
+            key={"bold"}
+            className='rounded-full px-3 py-1 text-sm/6 font-semibold text-gray-900 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-gray-900 data-hover:bg-gray-100 data-selected:bg-gray-200 data-selected:data-hover:bg-gray-200'
+          >
+            Iconsax Bold
+          </Tab>
+        </TabList>
+        <TabPanels className='mt-3'>
+          <TabPanel key={"linear"} className='rounded-xl bg-gray-50 p-3'>
+            <div className='bg-n-0 w-[1300px] h-full py-16 mx-auto'>
+              <p className='body1 font-bold text-center'>
+                Total Iconsax Complete From Figma : {iconNames2?.length}
+              </p>
+              <p className='body1 font-bold text-center'>
+                Iconsax&apos;s Complete Icons
+              </p>
+              <div className='grid grid-cols-5 px-10 py-10 gap-10 place-items-start'>
+                {iconNames2?.map((icon: string, index: number) => {
+                  return (
+                    <div key={index} className='col-span-1 flex flex-row gap-4'>
+                      <Iconsax2 name={icon as any} />
+                      <p>{icon}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </TabPanel>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <TabPanel key={"bold"} className='rounded-xl bg-gray-50 p-3'>
+            <div className='bg-n-0 w-[1300px] h-full py-16 mx-auto'>
+              <p className='body1 font-bold text-center'>
+                Total Iconsax BOLD Complete From Figma :{" "}
+                {iconNamesBold2?.length}
+              </p>
+              <p className='body1 font-bold text-center'>
+                Iconsax&apos;s Icons
+              </p>
+              <div className='grid grid-cols-5 px-10 py-10 gap-10 place-items-start'>
+                {iconNamesBold2?.map((icon: string, index: number) => {
+                  return (
+                    <div key={index} className='col-span-1 flex flex-row gap-4'>
+                      <IconsaxBold2 name={icon as any} />
+                      <p>{icon}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
+  );
 }
